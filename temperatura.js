c880286fd43574f17c32395795ec3af8b31b86b6
Vitória Xavier <vitoriapxaviera@gmail.com>
2025-05-06 15:08:21 -0300

    Exercício temperatura

---
+++ temperatura.js
@@ -0,0 +1,5 @@
+let temperaturaCentigrado = 34
+
+let temperaturaFahrenheint = ( 9 * temperaturaCentigrado + 160) / 5
+
+console.log("34 graus Centígrados em Fahrenheint: " , temperaturaFahrenheint)
