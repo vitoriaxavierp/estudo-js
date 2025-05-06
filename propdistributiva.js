8fc6128f6b9d75f0f90b17e2cdae9a2b10f6017e
Vitória Xavier <vitoriapxaviera@gmail.com>
2025-05-06 15:10:32 -0300

    Exercício Distributiva

---
+++ propdistributiva.js
@@ -0,0 +1,11 @@
+let A = 9
+let B = 3.7
+let C = 2
+let D = 8.5
+
+console.log("Soma de A com B = ", (A + B) , "Multiplicação de A com B = " , (A * B))
+console.log("Soma de A com C = " ,(A + C) , "Multiplicação de A com C = " , (A * C))
+console.log("Soma de A com D = ",(A + D) , "Multiplicação de A com D = " , (A * D))
+console.log("Soma de B com C = ",(B + C) , "Multiplicação de B com C = " , (B * C))
+console.log("Soma de B com D = ",(B + D) , "Multiplicação de B com D = " , (B * D))
+console.log("Soma de C com D = ",(C + D) , "Multiplicação de C com D = " , (C * D))
