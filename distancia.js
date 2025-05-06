f2cc923baf96c24772618e2ad4ae78c3f4da536f
Vitória Xavier <vitoriapxaviera@gmail.com>
2025-05-06 15:07:20 -0300

    Exercício distância

---
+++ distancia.js
@@ -0,0 +1,9 @@
+let tempo = 60
+let velocidade = 20
+
+let distancia = tempo * velocidade
+let litros_usados = distancia / 12 
+
+
+console.log("Velocidade média:" , velocidade , "\n Tempo gasto:" , tempo , "\n Distância" , distancia ,  "\n Quantidade de litros utilizada:" , litros_usados)
+
